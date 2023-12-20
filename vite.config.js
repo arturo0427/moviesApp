28e146284cbb7694e619cd5b1d70f22259f3d3ca
Arturo Muñoz <boomer_rap@hotmail.com>
2023-12-20 03:03:17 -0500

    Base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base:"https://arturo0427.github.io/moviesApp/",
+  base:"./",
   plugins: [react()],
 })
