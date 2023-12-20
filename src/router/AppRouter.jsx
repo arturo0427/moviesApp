8de8113bed7fc7182bf1330e798e2513500f77bc
Arturo Muñoz <boomer_rap@hotmail.com>
2023-12-20 01:15:57 -0500

    Vite config

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -13,7 +13,7 @@ export const AppRouter = () => {
             <Route path="/add" element={<AddMoviePage />} />
             <Route path="/movie/:id" element={<DetailMoviePage />} />
             <Route path="/error" element={<ErrorPage />} />
-            <Route path="/*" element={<Navigate to='/' />} />
+            <Route path="/*" element={<Navigate to='/error' />} />
         </Routes>
     )
 }
