c738eb90dfbeac05f63fb4f9bd76625c73dcf8f6
Arturo Muñoz <boomer_rap@hotmail.com>
2023-12-20 01:07:20 -0500

    Correccion de rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Router, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { HomePage, SearchPage, AddMoviePage, DetailMoviePage, ErrorPage } from "../pages";
 
 
@@ -9,10 +9,10 @@ export const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="search" element={<SearchPage />} />
-            <Route path="add" element={<AddMoviePage />} />
-            <Route path="movie/:id" element={<DetailMoviePage />} />
-            <Route path="error" element={<ErrorPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/add" element={<AddMoviePage />} />
+            <Route path="/movie/:id" element={<DetailMoviePage />} />
+            <Route path="/error" element={<ErrorPage />} />
             <Route path="/*" element={<Navigate to='/error' />} />
         </Routes>
     )
